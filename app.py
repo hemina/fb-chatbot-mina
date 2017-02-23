634f45443022ff6b6891f4e90e8db16f256df169
何米娜 <hemina@heminadeMacBook-Pro.local>
2017-02-23 17:29:41 +0100

    debug

--- app.py
+++ app.py
@@ -38,7 +38,11 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    try:
+                        message_text = messaging_event["message"]["text"]  # the message's text
+
+                    except KeyError:
+                        message_text = "smile"
 
                     kernel = aiml.Kernel()
 
