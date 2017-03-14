df834285c3949003d201ed4ec7801fe5a61ac5ee
何米娜 <hemina@heminadeMacBook-Pro.local>
2017-03-14 19:09:18 +0100

    ready bilingue for heroku

--- app.py
+++ app.py
@@ -210,10 +210,8 @@ def verify():
     # the 'hub.challenge' value it receives in the query arguments
     
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        #global counter
-        #counter = 0
-        # if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-        #     return "Verification token mismatch", 403
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
     return "Hello world", 200
